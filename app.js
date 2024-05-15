a56ad390cfc8d07b9aff2ebc96a213d654e1bc28
Aman Dangol <amandangol423@gmail.com>
2024-05-15 14:06:55 +0545

    insert query complete

--- app.js
+++ app.js
@@ -12,11 +12,20 @@ let connection = mysql.createConnection({
 server.set("view engine", "ejs");
 
 server.use("/public", express.static("./public"));
+server.use(express.urlencoded({ extended: false }));
 
 server.get("/", (req, res) => {
   let data = connection.query("select * from expenses", (err, data, fields) => {
     res.render("index", { data: data });
   });
+});
+
+server.post("/insert", (req, res) => {
+  const { title, category, amount } = req.body;
+  connection.query(
+    `insert into expenses(title,category,amount) values('${title}','${category}','${amount}')`
+  );
+  res.redirect("/");
 });
 
 server.listen(8000);
